15650efb1d072e7438dcb4064ec5e26f923eeda0
b-kucherepa <bkuchr@gmail.com>
2024-06-19 19:33:48 +0400

    feat: change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.wedjatreader.app',
-  appName: 'wedjat-reader',
+  appName: 'Wedjat Reader',
   webDir: 'build/'
 };
 
